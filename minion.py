ce1733560c63d7952b2d0c9505565f29b2ac88f7
ciechowoj <keepitsimplesirius@gmail.com>
2016-03-01 17:41:38 +0100

    Adds regex search.

--- minion.py
+++ minion.py
@@ -1,5 +1,5 @@
 import sublime, sublime_plugin
-import subprocess
+import subprocess, re, os, os.path
 
 class KillMasterCommand(sublime_plugin.ApplicationCommand):
 	def run(self):
@@ -21,11 +21,67 @@ def expand_variables_ex(value, variables):
 	else:
 		return value
 			
+class Panel:
+	def __init__(self, view):
+		self.view = view
+
+	def __getattr__(self, name):
+		return getattr(self.view, name)
+
+	def clear(self):
+		self.run_command("minion_clear_view")
+
+	def append(self, text):
+		self.run_command("minion_panel_append", { "text" : text })
+
+	@classmethod
+	def find_panel(cls, window, name):
+		panel = Panel(window.find_output_panel(name))
+		window.run_command("show_panel", { "panel" : "output.{}".format(name) })
+		return panel
+
+	@classmethod
+	def find_exec_panel(cls, window):
+		return Panel.find_panel(window, "exec")
+
 class MinionClearViewCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		print("Erasing...")
-		print(self.view.size())
-		self.view.insert(edit, 0, "Hello World!")
+		self.view.erase(edit, sublime.Region(0, self.view.size()))
+
+class MinionPanelAppendCommand(sublime_plugin.TextCommand):
+	def run(self, edit, text):
+		self.view.insert(edit, self.view.size(), text)
+
+class MinionNextResultCommand(sublime_plugin.WindowCommand):
+	def __init__(self, window):
+		super().__init__(window)
+		self.position = 0
+
+	def run(self, action = None, **kwargs):
+		if action == "init":
+			self.position = 0
+			self.build_system = kwargs["build_system"]
+		else:
+			panel = Panel.find_exec_panel(self.window)
+			file_regex = self.build_system["file_regex"]
+			region = panel.find(file_regex, self.position)
+
+			self.position = region.end()
+			message = panel.substr(region)
+
+			match = re.match(file_regex, message)
+			file = match.group(1)
+			line = match.group(2)
+			column = match.group(3)
+
+			working_dir = self.build_system["working_dir"]
+			basename = "{}:{}:{}".format(file, line, column)
+			path = os.path.join(working_dir, basename)
+
+			self.window.open_file(path, sublime.ENCODED_POSITION)
+			print("{}({}:{})".format(file, line, column))
+			print(file_regex, panel.substr(region))
+			
 
 class MinionBuildCommand(sublime_plugin.WindowCommand):
 	def __init__(self, *args):
@@ -33,6 +89,7 @@ class MinionBuildCommand(sublime_plugin.WindowCommand):
 
 		self.build_system = None
 		self.build_process = None
+		self.build_result_position = None
 
 	def __del__(self):
 		if self.build_process:
@@ -59,13 +116,13 @@ class MinionBuildCommand(sublime_plugin.WindowCommand):
 		return build_systems
 
 	def open_output_panel(self):
-		output = self.window.create_output_panel("exec")
+		panel = Panel(self.window.create_output_panel("exec"))
 		self.window.run_command("show_panel", { "panel" : "output.exec" })
-		output.run_command("minion_clear_view")
-		return output
+		panel.clear()
+		return panel
 
 	def run_build(self, build_system):
-		print(build_system["name"])
+		self.window.run_command("save_all")
 
 		if self.build_process != None:
 			self.build_process.terminate()
@@ -77,8 +134,13 @@ class MinionBuildCommand(sublime_plugin.WindowCommand):
 				stdout = subprocess.PIPE, 
 				stderr = subprocess.STDOUT)
 
-			output = self.open_output_panel()
-			
+			self.build_result_position = 0
+
+			panel = self.open_output_panel()
+			self.window.run_command("minion_next_result", { "action" : "init", "build_system" : build_system })
+
+			for line in self.build_process.stdout:
+				panel.append(line.decode("utf-8"))
 
 		sublime.set_timeout_async(callback, 0)
 
